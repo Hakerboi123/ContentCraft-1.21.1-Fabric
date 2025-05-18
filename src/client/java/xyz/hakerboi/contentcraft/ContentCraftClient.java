448949b846af6be9dd51e2f23fba4289da298bf2
Hakerboi <racerburhan@gmail.com>
2025-05-18 20:49:41 +0100

    Basic Setup

--- src/client/java/xyz/hakerboi/contentcraft/ContentCraftClient.java
+++ src/client/java/xyz/hakerboi/contentcraft/ContentCraftClient.java
@@ -1,10 +1,16 @@
 package xyz.hakerboi.contentcraft;
 
 import net.fabricmc.api.ClientModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import static xyz.hakerboi.contentcraft.ContentCraft.MOD_ID;
+
 
 public class ContentCraftClient implements ClientModInitializer {
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 	@Override
 	public void onInitializeClient() {
-		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+		LOGGER.info("Initialised Client!");
 	}
 }
