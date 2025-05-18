5797604d10510cd89701ec050d9a7e9ccd978474
Hakerboi <racerburhan@gmail.com>
2025-05-18 20:18:20 +0100

    Initial commit-haker

---
+++ src/client/java/xyz/hakerboi/contentcraft/ContentCraftClient.java
@@ -0,0 +1,10 @@
+package xyz.hakerboi.contentcraft;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ContentCraftClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
---
+++ src/client/java/xyz/hakerboi/contentcraft/ContentCraftDataGenerator.java
@@ -0,0 +1,11 @@
+package xyz.hakerboi.contentcraft;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class ContentCraftDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
